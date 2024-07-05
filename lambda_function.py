5e3943c37998b000726b56c1f4437370a86ac1ea
Dr-Banana <zhangxuanzhi10419@gmail.com>
2024-07-05 11:21:09 -0700

    Revert "update json input"
    This reverts commit 16f382a8eb2616de769387e09fd5df764a1298be.

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,11 @@
+import pandas as pd
 import json
 
 def lambda_handler(event, context):
+    d = {'col1': [1,2], 'col2': [3,4]}
+    df = pd.DataFrame(data=d)
+    print(df)
+    print('Done x1.1')
     if event and isinstance(event, dict):
         input_data = event
     elif event and isinstance(event, str):
--- test_lambda_function.py
+++ test_lambda_function.py
@@ -1,12 +1,12 @@
-import unittest
 from lambda_function import lambda_handler
+import unittest
 import io
 import sys
 import json
 
 class TestLambdaFunction(unittest.TestCase):
     def test_lambda_handler(self):
-        # 捕获标准输出
+
         captured_output = io.StringIO()
         sys.stdout = captured_output
         
@@ -14,6 +14,7 @@ class TestLambdaFunction(unittest.TestCase):
         test_input = json.dumps({"a": "aaaa", "b": "bbbb", "c": "cccc"})
         
         # 调用 lambda_handler
+        lambda_handler(None, None)
         lambda_handler(test_input, None)
         
         # 恢复标准输出
@@ -21,6 +22,7 @@ class TestLambdaFunction(unittest.TestCase):
         
         # 检查输出
         output = captured_output.getvalue()
+        self.assertIn("Done x1.1", output)
         self.assertIn("aaaa", output)
 
 if __name__ == '__main__':
